840ceb0a9767b2f910607fa894b0795066c38332
PradnyaKarmalkar <pradnyakarmalkar081@gmail.com>
2025-05-14 19:27:54 +0530

    added 4 api's

---
+++ main.py
@@ -0,0 +1,45 @@
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+tasks=[]
+
+class Task(BaseModel):
+    name: str
+    
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get('/hello123')
+def hello():
+    return {'msg':'hello from hello123'}
+
+@app.post('/create-task')
+def create_task(task: Task):
+    print(task)
+    tasks.append(task)
+    return {'msg':"Task added Successfully"}
+
+@app.get('/get-task')
+def get_task():
+    return {"tasks":tasks}
+
+@app.put('/update-task/{task_id}')
+def update_task(task_id: int, task: Task):
+    if task_id < 0 or task_id >= len(tasks):
+        return { 'msg': 'Task not found' }
+    tasks[task_id] = task
+    return { 'msg': 'Task updated succesfully' }
+
+@app.delete('/delete-task/{task_id}')
+def delete_task(task_id: int):
+    if task_id < 0 or task_id >= len(tasks):
+        return { 'msg': 'Task not found' }
+    tasks.pop(task_id)
+    return { 'msg': 'Task deleted succesfully' }
